2bbde882eeec8887952dc56b429ee4138bd6f2a9
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2021-04-16 19:35:55 +0530

    testing....

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ let cachedTags
  */
 module.exports.syncSkills = async (event, context, callback) => {
   let tagsResponse
+  console.log('.....start......')
   if (!cachedTags) {
     tagsResponse = await tag.getTags()
     if (tagsResponse.statusCode != 200) {
