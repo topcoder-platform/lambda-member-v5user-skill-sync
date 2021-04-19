2dcddfd8266911a66b57386420eb8703bc239e65
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2021-04-19 22:54:10 +0530

    To avoid looping of DB streamning on any unknown/uncaught error

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const helper = require('./src/common/helper')
  * @param {boolean} isDelete if delete the skill
  * @returns {Promise}
  */
-async function syncUserSkill (userId, tagId, score, skillProviderId, isDelete) {
+async function syncUserSkill(userId, tagId, score, skillProviderId, isDelete) {
   const name = await helper.getTagName(tagId)
   const skill = await helper.getUbahnResource('skills', { skillProviderId, name })
   const skillExist = await helper.checkUserSkillExist(userId, skill.id)
@@ -33,38 +33,42 @@ async function syncUserSkill (userId, tagId, score, skillProviderId, isDelete) {
  * @param {Object} event the event object
  */
 module.exports.handle = async (event) => {
-  console.log(`Received event: `, JSON.stringify(event))
-  const skillProvider = await helper.getUbahnResource('skillsProviders', { name: config.SKILL_PROVIDER_NAME })
-  for (const record of event.Records) {
-    try {
-      const handle = _.get(record, 'dynamodb.NewImage.userHandle.S')
-      const newSkills = JSON.parse(_.get(record, 'dynamodb.NewImage.skills.S', '{}'))
-      const oldSkills = JSON.parse(_.get(record, 'dynamodb.OldImage.skills.S', '{}'))
-      const createSkills = _.omit(newSkills, _.keys(oldSkills))
-      const deleteSkills = _.omit(oldSkills, _.keys(newSkills))
-      const updateSkills = _.filter(_.pick(newSkills, _.keys(oldSkills)), (v, k) => oldSkills[k].score !== v.score)
-      if (_.isEmpty(createSkills) && _.isEmpty(deleteSkills) && _.isEmpty(updateSkills)) {
-        console.log(`there are no skills to create, update or delete for user ${handle}`)
-        continue
+  try {
+    console.log(`Received event: `, JSON.stringify(event))
+    const skillProvider = await helper.getUbahnResource('skillsProviders', { name: config.SKILL_PROVIDER_NAME })
+    for (const record of event.Records) {
+      try {
+        const handle = _.get(record, 'dynamodb.NewImage.userHandle.S')
+        const newSkills = JSON.parse(_.get(record, 'dynamodb.NewImage.skills.S', '{}'))
+        const oldSkills = JSON.parse(_.get(record, 'dynamodb.OldImage.skills.S', '{}'))
+        const createSkills = _.omit(newSkills, _.keys(oldSkills))
+        const deleteSkills = _.omit(oldSkills, _.keys(newSkills))
+        const updateSkills = _.filter(_.pick(newSkills, _.keys(oldSkills)), (v, k) => oldSkills[k].score !== v.score)
+        if (_.isEmpty(createSkills) && _.isEmpty(deleteSkills) && _.isEmpty(updateSkills)) {
+          console.log(`there are no skills to create, update or delete for user ${handle}`)
+          continue
+        }
+        console.log('Skills to create:', JSON.stringify(createSkills))
+        console.log('Skills to update:', JSON.stringify(updateSkills))
+        console.log('Skills to delete:', JSON.stringify(deleteSkills))
+        const user = await helper.getUser(handle)
+        for (const key of _.keys(createSkills)) {
+          await syncUserSkill(user.id, key, createSkills[key].score, skillProvider.id)
+          await helper.sleep()
+        }
+        for (const key of _.keys(updateSkills)) {
+          await syncUserSkill(user.id, key, createSkills[key].score, skillProvider.id)
+          await helper.sleep()
+        }
+        for (const key of _.keys(deleteSkills)) {
+          await syncUserSkill(user.id, key, 0, skillProvider.id, true)
+          await helper.sleep()
+        }
+      } catch (e) {
+        console.log(`sync user's skills failed. Error: ${e.message}`)
       }
-      console.log('Skills to create:', JSON.stringify(createSkills))
-      console.log('Skills to update:', JSON.stringify(updateSkills))
-      console.log('Skills to delete:', JSON.stringify(deleteSkills))
-      const user = await helper.getUser(handle)
-      for (const key of _.keys(createSkills)) {
-        await syncUserSkill(user.id, key, createSkills[key].score, skillProvider.id)
-        await helper.sleep()
-      }
-      for (const key of _.keys(updateSkills)) {
-        await syncUserSkill(user.id, key, createSkills[key].score, skillProvider.id)
-        await helper.sleep()
-      }
-      for (const key of _.keys(deleteSkills)) {
-        await syncUserSkill(user.id, key, 0, skillProvider.id, true)
-        await helper.sleep()
-      }
-    } catch (e) {
-      console.log(`sync user's skills failed. Error: ${e.message}`)
     }
+  } catch (e) {
+    console.log(`some error occured: ${e.message}`)
   }
 }
